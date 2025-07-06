1c11e8ad534e24604baa7980701b979e929d81ea
Mahdizynali <zeinali.mahdy@gmail.com>
2025-07-06 22:36:50 +0330

    ad classes

--- utils/config.py
+++ utils/config.py
@@ -14,23 +14,27 @@ STEMMER_SUFFIX_PATH = "manipulate/stem_suffixes.txt"
 STEMMER_PREFIX_PATH = "manipulate/stem_prefixes.txt"
 
 English_categories = {
-    "sad": 0,
-    "fear": 1,
-    "surprise": 2,
-    "disgust": 3,
-    "anger": 4,
-    "joy": 5
+    "sport": 0,
+    "politics": 1,
+    "internatial": 2,
+    "social": 3,
+    "economical": 4,
+    "cultural": 5,
+    "science": 6,
+    "events": 7
 }
 
 Persian_categories = {
-    0: "ناراحت",
-    1: "ترس",
-    2: "شگفت زده",
-    3: "منزجر",
-    4: "عصبانیت",
-    5: "ترکیبی"
+    0: "ورزشی",
+    1: "سیاسی",
+    2: "بین الملل",
+    3: "اجتماعی",
+    4: "اقتصادی",
+    5: "فرهنگی",
+    5: "علمی",
+    7: "حوادث"
 }
 
 LEARNING_RATE=0.01
-CLASSES=6 # تعداد عواطف بر اساس دیتاست
+CLASSES=7 
 ITRATION=1
