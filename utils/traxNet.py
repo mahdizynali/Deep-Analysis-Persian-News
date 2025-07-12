38f31b8727ad3d5903e68eb2ee8ffae3a9aa00ce
Mahdizynali <zeinali.mahdy@gmail.com>
2025-07-13 01:06:43 +0330

    fix minor issue

--- utils/traxNet.py
+++ utils/traxNet.py
@@ -6,7 +6,7 @@ class newsModel():
         self.vcs = vocab_size
         self.Model()
 
-    def Model(self, , d_model = 128):
+    def Model(self, d_model = 128):
         model_sequence = tl.Serial(
             tl.Embedding(self.vcs, d_feature=d_model),
             tl.LSTM(d_model),
