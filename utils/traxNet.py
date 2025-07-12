b6bf1428afdce971bc16cb7696b2b9f6744f8dfb
Mahdizynali <zeinali.mahdy@gmail.com>
2025-07-13 01:12:30 +0330

    convert to simple function

--- utils/traxNet.py
+++ utils/traxNet.py
@@ -1,21 +1,14 @@
 from .config import *
 
-class newsModel():
-
-    def __init__ (self, vocab_size) -> None :
-        self.vcs = vocab_size
-        self.Model()
-
-    def Model(self, d_model = 128):
-        model_sequence = tl.Serial(
-            tl.Embedding(self.vcs, d_feature=d_model),
-            tl.LSTM(d_model),
-            tl.Mean(axis=1),
-            tl.Dense(128),
-            tl.Relu(),
-            tl.Dense(64),
-            tl.Relu(),
-            tl.Dense(CLASSES),
-            tl.LogSoftmax()
-        )
-        return model_sequence
+def newsModel(vocab_size, d_model=128):
+    return tl.Serial(
+        tl.Embedding(vocab_size, d_feature=d_model),
+        tl.LSTM(d_model),
+        tl.Mean(axis=1),
+        tl.Dense(128),
+        tl.Relu(),
+        tl.Dense(64),
+        tl.Relu(),
+        tl.Dense(CLASSES),
+        tl.LogSoftmax()
+    )
